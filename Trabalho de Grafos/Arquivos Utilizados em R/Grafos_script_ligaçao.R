75dc50c7baba99895e4f35a371b530df4e58f9dd
João Pedro Moura <72622132+Jopei@users.noreply.github.com>
2023-08-01 09:59:25 -0300

    Uso de Python e R

---
+++ Trabalho de Grafos/Arquivos Utilizados em R/Grafos_script_ligaçao.R
@@ -0,0 +1,22 @@
+---
+title: "R Notebook"
+output: html_notebook
+---
+  #install.packages("igraph", dependencies = T)
+library(igraph)
+
+g1 = graph(c("Deputada", "D29", 
+             "Deputada", "D31", 
+             "Deputada", "D32", 
+             "Deputada", "D33", 
+             "Deputada", "D34", 
+             "Deputada", "D35", 
+             "Deputada", "D36", 
+             "Deputada", "D37",
+             "D34", "D5","D5", "D433","D433", "D6", 
+             "D6", "D36","D36", "Deputada"
+             
+             ))
+
+ plot(g1, edge.arrow.size = .1, vertex.size = 18, edge.curved = 0.1)
+
---
+++ Trabalho de Grafos/Arquivos Utilizados em R/Usuarios_com_Nome.py
@@ -0,0 +1,45 @@
+"Carla Zambelli", "Danilo P", "Carla Zambelli", "eliane", "Carla Zambelli", "Adriano",
+"Carla Zambelli", "Hislane ", "Carla Zambelli", "Antonio", "Carla Zambelli", "Silvia",
+"Carla Zambelli", "Luis A", "Carla Zambelli", "Erinaldo R", "Carla Zambelli", "RLeão",
+"Carla Zambelli", "Benedicto L", "Carla Zambelli", "Luiz T", "Carla Zambelli", "Tarciso Junior ",
+"Carla Zambelli", "Jorge L", "Carla Zambelli", "Emerson Mauro ", "Carla Zambelli", "Marcelo C",
+"Carla Zambelli", "Leo D", "Carla Zambelli", "Tiago L ", "Carla Zambelli", "Fernando S",
+"Carla Zambelli", "Taylan", "Carla Zambelli", "Alessando M", "Carla Zambelli", "Jadson M",
+"Carla Zambelli", "Iranildo dos Santos", "Carla Zambelli", "Jose Gimenez", "Carla Zambelli", "Jefesson",
+"Carla Zambelli", "Paulo 135", "Carla Zambelli", "Tania", "Carla Zambelli", "Flavio S",
+"Carla Zambelli", "Marcio Sperber", "Carla Zambelli", "Jalson F",  "Carla Zambelli", "José M",
+"Carla Zambelli", "Joaquim ", "Carla Zambelli", "Narogem ", "Carla Zambelli", "Dan ",
+"Carla Zambelli", "Carlos S", "Carla Zambelli", "Karlao", "Carla Zambelli", "Reynaldo", 
+"Carla Zambelli", "ICalheiros", "Carla Zambelli", "Jeff", "Carla Zambelli", "ClaudioM",
+"Carla Zambelli", "Paulo A", "Carla Zambelli", "João B", "Carla Zambelli", "Patthy B",
+"Carla Zambelli", "Euzinha", "Carla Zambelli", "Dirceu", "Carla Zambelli", "Jose J",
+"Carla Zambelli", "Maria V","Carla Zambelli", "Joelma B", "Carla Zambelli", "Renato A",
+"Carla Zambelli", "Marcos S","Carla Zambelli", "@akino", "Carla Zambelli", "Carmen S",
+"Carla Zambelli", "Linds", "Carla Zambelli", "APT", "Carla Zambelli", "Allisson ",
+"Carla Zambelli", "Leonardo C", "Carla Zambelli", "Carla 19", "Carla Zambelli", "Antonio A",
+"Carla Zambelli", "Erinaldo R", "Carla Zambelli", "Leonardo M", "Carla Zambelli", "José R",
+"Carla Zambelli", "Julio C", "Carla Zambelli", "Renato D", "Carla Zambelli", "Roberto R",
+"Carla Zambelli", "Eduardo C", "Carla Zambelli", "Andre R", "Carla Zambelli", "TaniDu",
+"Carla Zambelli", "Lucia R", "Carla Zambelli", "JPMartins ", "Carla Zambelli", "Solange",
+"Carla Zambelli", "Donaldo ", "Carla Zambelli", "Fernando R", "Carla Zambelli", "Devinir",
+"Carla Zambelli", "Sandra Regina ", "Carla Zambelli", "Harlei N", "Carla Zambelli", "Eneida G",
+"Carla Zambelli", "Adonias M", "Carla Zambelli", "Silvio C", "Carla Zambelli", "Dimas Batalha",
+"Carla Zambelli", "Izabel M", "Carla Zambelli", "Mauri Jose", "Carla Zambelli", "Arnobio Alves",
+"Carla Zambelli", "Diomar", "Carla Zambelli", "Rubinho S", "Carla Zambelli", "Sandra O"
+
+"Esquerda"
+
+"Carla Zambelli","Lika", "Carla Zambelli","Mamão", "Carla Zambelli","Franz",
+"Carla Zambelli","Marvin", "Carla Zambelli","Chester", "Carla Zambelli","Sulcrilhos",
+"Carla Zambelli","Jussara T", "Carla Zambelli","Vânia F ", "Carla Zambelli","Gracielle",
+"Carla Zambelli","Jucemar Cad", "Carla Zambelli","LF", "Carla Zambelli","Feélix Enzo ",
+"Carla Zambelli","Wagner", "Carla Zambelli","Olion", "Carla Zambelli","L3Bron",
+"Carla Zambelli","Gelson B", "Carla Zambelli","Adalberto", "Carla Zambelli","Marco Matos",
+"Carla Zambelli","Genera", "Carla Zambelli","Alex ", "Carla Zambelli","Edilene",
+"Carla Zambelli","Alberto H", "Carla Zambelli","Davi Jorge"
+
+"Centro"
+
+"Carla Zambelli","Caleb", "Carla Zambelli","Edison", "Carla Zambelli","Regina F",
+"Carla Zambelli","Rosali M", "Carla Zambelli","Sergio Costa", "Carla Zambelli","Francisco C",
+"Carla Zambelli","Rayrene Medeiros", "Carla Zambelli","Marco Antonio"
